ca136e7fd2a33be6e0b10d0c0303d04a5d14c816
ulic-youthlic <ulic.youthlic@gmail.com>
2023-07-27 23:27:47 +0800

    Add indent balnkline. Modify lua/plugins/ui.lua.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,10 +1,16 @@
 return {
-    -- 添加插件
-    "akinsho/bufferline.nvim",
-    -- 启用插件
-    config = {} -- 也可以写成 true
-    -- 等价于
-    -- config = function()
-    --     require("bufferline").setup({})
-    -- end
+    {
+        -- 添加插件
+        "akinsho/bufferline.nvim",
+        -- 启用插件
+        config = {} -- 也可以写成 true
+        -- 等价于
+        -- config = function()
+        --     require("bufferline").setup({})
+        -- end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = true
+    },
 }
