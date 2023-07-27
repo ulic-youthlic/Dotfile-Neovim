1b1e01dc135b171dea5f40faa23af50a49f976ca
ulic-youthlic <ulic.youthlic@gmail.com>
2023-07-27 23:44:27 +0800

    Add git signs. Modify lua/plugins/ui.lua.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,4 +13,25 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         config = true
     },
+    {
+        -- 安装 git 标志支持
+        "lewis6991/gitsigns.nvim",
+        config = {
+            -- 打开 line blame
+            current_line_blame = true,
+            -- 配置 line blame 选项
+            current_line_blame_opts = {
+                -- 显示虚拟文字
+                virt_text = true,
+                -- 设置 line blame 的位置为行末
+                virt_text_pos = 'eol',
+                -- 延迟为 100 ms
+                delay = 100,
+                -- 不忽略空白字符
+                ignore_whitespace = false,
+            },
+            -- 设置 line blame 显示格式
+            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        }
+    },
 }
