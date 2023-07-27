4ccdaa2c7db01d5efff95f65e6d4c70109038fa3
ulic-youthlic <ulic.youthlic@gmail.com>
2023-07-27 23:03:04 +0800

    Add status bar for theme. Add four plugins to lua/plugins/theme.lua.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,10 +2,34 @@
 return {
     -- 加入 tokyonight 主题
     "folke/tokyonight.nvim",
+    -- 安装与 tokyonight 相兼容的插件，可以不以依赖的形式安装
+    dependencies = {
+        -- 下方状态栏插件
+        "nvim-lualine/lualine.nvim",
+        -- 为状态栏提供图标的插件
+        "nvim-tree/nvim-web-devicons",
+        -- 上方标题栏插件
+        "utilyre/barbecue.nvim",
+        -- 被标题栏插件依赖
+        "SmiteshP/nvim-navic",
+    },
     -- 在加载插件时的 config 阶段会自动运行 function
     config = function()
         -- 设置 Neovim 主题为 tokyonigh-storm
         ---- :vim.cmd(): 执行 Vim Script 并返回值
         vim.cmd[[colorscheme tokyonight-storm]]
+        -- 加载 lualine 插件
+        require('lualine').setup({
+            -- 更改配置
+            options = {
+                -- 设置主题为 tokyonight
+                theme = 'tokyonight'
+            },
+        })
+        -- 启用上方标题栏插件
+        require('barbecue').setup {
+            -- 设置主题为 tokyonight
+            theme = 'tokyonight',
+        }
     end
 }
