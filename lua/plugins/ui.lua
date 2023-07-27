91a3e32ba5225f0b8258dc89b2c841fa28336b39
ulic-youthlic <ulic.youthlic@gmail.com>
2023-07-27 23:18:57 +0800

    Add buffer line. Add file, lua/plugins/ui.lua.

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,10 @@
+return {
+    -- 添加插件
+    "akinsho/bufferline.nvim",
+    -- 启用插件
+    config = {} -- 也可以写成 true
+    -- 等价于
+    -- config = function()
+    --     require("bufferline").setup({})
+    -- end
+}
