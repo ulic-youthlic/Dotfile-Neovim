4337b538043f00b688dac6f39dd4e43741c00d86
ulic-youthlic <ulic.youthlic@gmail.com>
2023-07-27 21:12:51 +0800

    Add theme, Tokyonight(Modify lua/lazy_nvim.lua).

--- lua/lazy_nvim.lua
+++ lua/lazy_nvim.lua
@@ -19,10 +19,26 @@
 vim.opt.rtp:prepend(lazypath)
 
 -- 列出所需插件的列表
-local plugins = {}
+local plugins = {
+    {
+        -- 加入 tokyonight 主题
+        "folke/tokyonight.nvim",
+        -- 在加载插件时的 config 阶段会自动运行 function
+        config = function()
+            -- 设置 Neovim 主题为 tokyonigh-storm
+            ---- :vim.cmd(): 执行 Vim Script 并返回值
+            vim.cmd[[colorscheme tokyonight-storm]]
+        end
+    },
+}
 
 -- 写出 lazy.nvim 的配置选项
-local opts = {}
+local opts = {
+    install = {
+        -- 设置 lazy.nvim 的主题为 tokyonight-storm
+        colorscheme = { "tokyonight-storm" },
+    },
+}
 
 -- 使用 lazy.nvim 安装插件
 require("lazy").setup(plugins, opts)
