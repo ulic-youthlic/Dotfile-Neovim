e5960c47c6ef9d6c413ab8b18b47adec7c242e43
ulic-youthlic <Ulic.youthlic@gmail.com>
2023-08-03 12:54:04 +0800

    Add a plugin for multi-cursor's mode.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -120,4 +120,47 @@ return {
         "echasnovski/mini.comment",
         config = true
     },
+    {
+        "smoka7/multicursors.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            'smoka7/hydra.nvim',
+        },
+        config = function()
+            local N = require 'multicursors.normal_mode'
+            local I = require 'multicursors.insert_mode'
+            require("multicursors").setup(
+                {
+                    normal_keys = {
+                        -- to change default lhs of key mapping change the key
+                        [','] = {
+                            -- assigning nil to method exits from multi cursor mode
+                            method = N.clear_others,
+                            -- you can pass :map-arguments here
+                            opts = { desc = 'Clear others' },
+                        },
+                    },
+                    insert_keys = {
+                        -- to change default lhs of key mapping change the key
+                        ['<CR>'] = {
+                            -- assigning nil to method exits from multi cursor mode
+                            method = I.Cr_method,
+                            -- you can pass :map-arguments here
+                            opts = { desc = 'New line' },
+                        },
+                    },
+                }
+            )
+            vim.keymap.set({ "n", "v" }, "<C-m>", "<cmd>MCstart<CR>", { desc = "Start multi cursors mode" })
+        end,
+        cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+        keys = {
+            {
+                mode = { 'v', 'n' },
+                '<Leader>m',
+                '<cmd>MCstart<cr>',
+                desc = 'Create a selection for selcted text or word under the cursor',
+            },
+        },
+    }
 }
